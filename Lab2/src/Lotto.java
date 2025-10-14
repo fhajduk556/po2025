fe0c61661a25732e63acae7f7ec243203d91211a
Franciszek Hajduk <fhajduk@student.agh.edu.pl>
2025-10-14 18:14:30 +0200

    lab2 wprowadzanie ze sprawdzaniem

--- Lab2/src/Lotto.java
+++ Lab2/src/Lotto.java
@@ -1,18 +1,24 @@
 import java.util.Random;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Lotto {
     public static void main(String[] args) {
-        wyswietl(losuj());
+        wyswietl(wprowadz());
+    }
+
+    public static ArrayList<Integer> number_base() {
+        ArrayList<Integer> available_numbers = new ArrayList<Integer>();
+        for (int i = 1; i <= 49; i++) {
+            available_numbers.add(i);
+        }
+        return available_numbers;
     }
 
     public static ArrayList<Integer> losuj() {
         ArrayList<Integer> winning_numbers = new ArrayList<Integer>();
         Random random = new Random();
-        ArrayList<Integer> available_numbers = new ArrayList<Integer>();
-        for (int i = 1; i <= 49; i++) {
-            available_numbers.add(i);
-        }
+        ArrayList<Integer> available_numbers = number_base();
         for (int i = 0; i < 6; i++) {
             int number_index = random.nextInt(available_numbers.size());
             winning_numbers.add(available_numbers.get(number_index));
@@ -22,11 +28,30 @@ public class Lotto {
     }
 
     public static void wyswietl(ArrayList<Integer> numbers) {
-        System.out.println("oto zwycięskie numery: ");
+        System.out.println("oto numery: ");
         for (int i = 0; i < numbers.size(); i++) {
             System.out.print(numbers.get(i) + " ");
         }
         System.out.println();
+    }
+
+    public static ArrayList<Integer> wprowadz() {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> available_numbers = number_base();
+        ArrayList<Integer> user_numbers = new ArrayList<Integer>();
+        System.out.println("Wprowadz swoje numery: ");
+        for (int i = 0; i < 6; i++) {
+            System.out.printf("Numer %d: \n", i + 1);
+            int numer = sc.nextInt();
+            if(available_numbers.contains(numer)) {
+                user_numbers.add(numer);
+                available_numbers.remove(Integer.valueOf(numer));
+            } else {
+                i = i - 1;
+                System.out.println("Wybierz numer od 1 do 49 jeszcze nie wybrany");
+            }
+        }
+        return user_numbers;
     }
 }
 
