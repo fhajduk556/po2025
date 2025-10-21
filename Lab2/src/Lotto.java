c903fcb95e6f1d612934c0ebe828f25e493af0d9
Franciszek Hajduk <fhajduk@student.agh.edu.pl>
2025-10-21 14:00:43 +0200

    lab2 done

--- Lab2/src/Lotto.java
+++ Lab2/src/Lotto.java
@@ -4,7 +4,46 @@ import java.util.Scanner;
 
 public class Lotto {
     public static void main(String[] args) {
-        wyswietl(wprowadz());
+        ArrayList<Integer> wining_numbers = losuj();
+        ArrayList<Integer> user_numbers = makeArray(args);
+        if (user_numbers == null) {
+            user_numbers = wprowadz();
+        }
+        System.out.println("trafień: " + String.valueOf(liczba_trafien(user_numbers,  wining_numbers)));
+        wyswietl(wining_numbers);
+        long t_start = System.currentTimeMillis();
+        int counter = 0;
+        while (liczba_trafien(user_numbers, wining_numbers) != 6) {
+            user_numbers = losuj();
+            counter++;
+        }
+        long t_stop = System.currentTimeMillis();
+        long czas = t_stop - t_start;
+        System.out.println("losowań: " + String.valueOf(counter));
+        System.out.println("czas: " + String.valueOf(czas));
+        return;
+    }
+
+    public static ArrayList<Integer> makeArray(String[] args) {
+        if (args.length != 6) {
+            return null;
+        }
+        ArrayList<Integer> user_numbers = new ArrayList<>();
+        ArrayList<Integer> available_numbers = number_base();
+        for (int i = 0; i < 6; i++) {
+            try{
+                int numer = Integer.parseInt(args[i]);
+                if (available_numbers.contains(numer)) {
+                    user_numbers.add(numer);
+                    available_numbers.remove(Integer.valueOf(numer));
+                } else {
+                    return null;
+                }
+            }catch(NumberFormatException e){
+                return null;
+            }
+        }
+        return user_numbers;
     }
 
     public static ArrayList<Integer> number_base() {
@@ -16,15 +55,15 @@ public class Lotto {
     }
 
     public static ArrayList<Integer> losuj() {
-        ArrayList<Integer> winning_numbers = new ArrayList<Integer>();
+        ArrayList<Integer> ran_numbers = new ArrayList<Integer>();
         Random random = new Random();
         ArrayList<Integer> available_numbers = number_base();
         for (int i = 0; i < 6; i++) {
             int number_index = random.nextInt(available_numbers.size());
-            winning_numbers.add(available_numbers.get(number_index));
+            ran_numbers.add(available_numbers.get(number_index));
             available_numbers.remove(number_index);
         }
-        return winning_numbers;
+        return ran_numbers;
     }
 
     public static void wyswietl(ArrayList<Integer> numbers) {
@@ -52,6 +91,11 @@ public class Lotto {
             }
         }
         return user_numbers;
+    }
+    public static int liczba_trafien(ArrayList<Integer> numbers, ArrayList<Integer> winning_numbers) {
+        ArrayList<Integer> numbers_copy = new ArrayList<>(numbers);
+        numbers_copy.retainAll(winning_numbers);
+        return numbers_copy.size();
     }
 }
 
