0eb49242b1c9b79829cec16f4c9dca574e39cb44
Franciszek Hajduk <fhajduk@student.agh.edu.pl>
2026-01-18 20:13:14 +0100

    gettery pozycji

--- Lab6/src/main/java/symulator/Pozycja.java
+++ Lab6/src/main/java/symulator/Pozycja.java
@@ -3,17 +3,24 @@ package symulator;
 public class Pozycja {
     private double x;
     private double y;
+
     public Pozycja(double x, double y) {
         this.x = x;
         this.y = y;
     }
+
     public void aktualizacja(double deltaX, double deltaY) {
         this.x = this.x + deltaX;
         this.y = this.y + deltaY;
     }
+
     public String getPozycja() {
         return String.valueOf(x) + "," + String.valueOf(y);
     }
+
+    public double getX() { return x; }
+    public double getY() { return y; }
+
     public void przenies(Pozycja cel, double V, double dt) {
         double dx = cel.x - this.x;
         double dy = cel.y - this.y;
